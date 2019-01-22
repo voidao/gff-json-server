6b10a4239557c718e8709284e3a8a4d313f5f757
sharphill <sharphill@163.com>
2019-01-22 14:59:34 +0800

    try fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const FileAsync = require('lowdb/adapters/FileAsync');
 const adapter = new FileAsync(filePath);
 const server = jsonServer.create();
 router = jsonServer.router(filePath);
-var db;
+//var db;
 const middlewares = jsonServer.defaults();
 
 const port = Number(process.env.PORT || 3000)
@@ -46,49 +46,53 @@ server.use(bodyParser.json())
 /*server.use(basicAuth({
     users: { 'gff': 'gffsecret' }
 })*/
-low(adapter)
+/*low(adapter)
   .then(dbAsync => {
     db = dbAsync;
     });
+*/
 
-server.use((req, res, next) => { // custom authorization here
+low(adapter)
+  .then(db => {
+        server.use((req, res, next) => { // custom authorization here
 
-    if (req.method == "GET") {
-        next() // continue to JSON Server router
-    } else if(req.method == "PATCH" && req.body.like) {
+        if (req.method == "GET") {
+            next() // continue to JSON Server router
+        } else if(req.method == "PATCH" && req.body.like) {
 
-        huntingObj = db.get('huntings')
-            .find({ id: req.body.like });
+            huntingObj = db.get('huntings')
+                .find({ id: req.body.like });
 
-        if(huntingObj) {
-            db.get('huntings')
-                .find({ id: req.body.like })
-                .assign({likes: {number: huntingObj.get('likes.number') + 1}})
-                .write().then(hunting => {
-                    res.status(200).json({
-                        id: hunting.get('id'),
-                        category: hunting.get('category'),
-                        likes: {number: hunting.get('likes.number')}
+            if(huntingObj) {
+                db.get('huntings')
+                    .find({ id: req.body.like })
+                    .assign({likes: {number: huntingObj.get('likes.number') + 1}})
+                    .write().then(hunting => {
+                        res.status(200).json({
+                            id: hunting.get('id'),
+                            category: hunting.get('category'),
+                            likes: {number: hunting.get('likes.number')}
+                        });
                     });
-                });
 
-            /*res.status(200).json({
-                id: huntingObj.get('id'),
-                category: huntingObj.get('category'),
-                likes: {number: huntingObj.get('likes.number')}
-            });*/
-        } else {
-            res.sendStatus(400)
-        }
-  } else {
-    res.sendStatus(401)
-  }
-})
-server.use(router);
-/*server.listen(port, () => {
-        console.log('JSON Server is running')
-})*/
+                /*res.status(200).json({
+                    id: huntingObj.get('id'),
+                    category: huntingObj.get('category'),
+                    likes: {number: huntingObj.get('likes.number')}
+                });*/
+            } else {
+                res.sendStatus(400)
+            }
+      } else {
+        res.sendStatus(401)
+      }
+    });
+    server.use(router);
+    /*server.listen(port, () => {
+            console.log('JSON Server is running')
+    })*/
 
-https.createServer(options, server).listen(port, function(){
-  console.log("JSON Server listening on port: " + port);
-});
+    https.createServer(options, server).listen(port, function(){
+      console.log("JSON Server listening on port: " + port);
+    });
+  });
